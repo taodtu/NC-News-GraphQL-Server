0c10198147e6da7b6061125dd199f66fb792b9f6
taodtu <taodtu@gmail.com>
2019-06-03 20:52:16 +0100

    initial commit

---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS nc_news_graphql_server;
+CREATE DATABASE nc_news_graphql_server;
+
+DROP DATABASE IF EXISTS nc_news_graphql_server_test;
+CREATE DATABASE nc_news_graphql_server_test;
