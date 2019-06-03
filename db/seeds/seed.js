4d979f34ee30ab76a2e6b364d13bff0bbcc932c0
taodtu <taodtu@gmail.com>
2019-06-03 21:21:28 +0100

    import the utils/format helper function

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,18 @@
-// const {  } = require('../data');
+const { topicData, userData, articleData, commentData } = require('../data');
+const { format } = require('../../utils/format')
 
-exports.seed = (knex, Promise) => {
-  return knex.migrate
-    .rollback()
-    .then(() => knex.migrate.latest())
-    .then(() => {
-      // insert data
-    });
+exports.seed = async (knex, Promise) => {
+  await knex.migrate.rollback()
+  await knex.migrate.latest();
+  await Promise.all([knex('topics').insert(topicData), knex('users').insert(userData)]);
+  const articles = await knex('articles')
+    .insert(articleData)
+    .returning('*');
+  const commentWithAuthor = commentData.map(comment => {
+    const { created_by, ...rest } = comment;
+    return { ...rest, author: created_by };
+  })
+  const formattedComment = format(articles, commentWithAuthor, "title", "article_id", "belongs_to")
+  await knex('comments')
+    .insert(formattedComment)
 };
