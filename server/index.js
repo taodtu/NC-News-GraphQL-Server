e55901d630875ce9abc935febed9740a19e360cd
taodtu <taodtu@gmail.com>
2019-06-10 20:20:12 +0100

    try config the path

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const server = new ApolloServer({
  }
 });
 
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app });
 
 app.listen({ port: 8000 }, () => {
  console.log('Apollo Server on http://localhost:8000/graphql');
