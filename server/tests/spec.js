73b0f731c93a31efbb4b8a4dbe263d71063bb354
taodtu <taodtu@gmail.com>
2019-06-10 21:55:33 +0100

    test module with axios

--- server/tests/spec.js
+++ server/tests/spec.js
@@ -1,5 +1,25 @@
-import { expect } from 'chai';
+process.env.NODE_ENV = 'test';
+const connection = require('../../db/connection');
+const { expect } = require('chai');
+const { topics } = require('./api');
 
-describe('articles', () => {
- it('articles')
-})
+describe('users', () => {
+ beforeEach(() => connection.seed.run());
+ after(() => connection.destroy());
+ describe('getUser(username:"grumpy19): User', () => {
+  it('returns a user when user can be found', async () => {
+   const expectedResult = {
+    data: {
+     getUser: {
+      username: "grumpy19",
+      name: "Paul Grump",
+     },
+    },
+   };
+
+   const result = await topics();
+
+   expect(result.data).to.eql(expectedResult);
+  });
+ });
+});
